a7835197c7c788a70de9752d10cd2d105687965b
Hideki Tsuchiya <htsuchiya@htsuchiya.local>
2019-05-18 10:40:38 +0900

    クエリパラメータを使ってconnpass APIにリクエストを投げるよう修正

--- app/controllers/api/connpass_controller.rb
+++ app/controllers/api/connpass_controller.rb
@@ -7,12 +7,8 @@ BASE_URI = 'https://connpass.com/api/v1/event/'
 
 class Api::ConnpassController < ApplicationController
   def event
-    params = {
-      series_id: 1712
-    }
-    req = HTTPClient.new.get(BASE_URI, params)
+    req = HTTPClient.new.get(BASE_URI, request.query_parameters)
     res = JSON.parse(req.body)
-    puts res
     render :json => res
   end
 end
